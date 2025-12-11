6b7e3f9c4acbe695eb1f90140bce676b87dbdb02
Bakhtiar Reza <69202556+Dradeon@users.noreply.github.com>
2025-12-11 00:51:54 +0000

    Hopefully this fixes it

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: "https://dradeon.github.io",
-    base: "/Personal-Site"
+    base: "/Personal-Site",
+    output: "static"
 });
